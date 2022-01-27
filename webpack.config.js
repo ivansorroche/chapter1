a690815d7df8f5bbd6af8e7591fe8ce1abda518c
Ivan <ivan@ivan.local>
2022-01-27 00:50:45 -0300

    01-github-explorer - projeto completo

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  //Quando da um aviso ou erro no console, mostra exatamente a linha que esta com problema
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    hot: true,
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin,
+    new HtmlWebpackPlugin({ 
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+    //Caso os isDevelopment retorne False o filter resolve, caso contrario daria erro pois o plugins não aceita false
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        }
+      },
+      //instalei o style-loader e o css-loader, isso faz que o meu arquivo App.jsx reconheca o arquivo global css
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ],
+  }
+};
